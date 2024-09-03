d0676fa5bd0dc3f574b179bf318a4750cb250086
Vikram-h <vikramarangapara15@gmail.com>
2024-09-03 13:15:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7222375251:AAE42CM2xffTOdJdyGTAOnZ90hbfYTAz9QM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@VIPBGIMFFBOT")
+OWNER_USERNAME=("VIKRAM RANGAPARA")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("6659991471")
